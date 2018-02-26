7d3d50d295283f4f48b420ab9e229b6f87fa9d6b
Maxime Carlier <maxime.mca@gmail.com>
2018-02-26 20:46:10 +0100

    Remove unused dependency and un-needed class

--- rimel-docker/src/main/java/fr/polytech/rimel/rimeldocker/api/GithubAPI.java
+++ rimel-docker/src/main/java/fr/polytech/rimel/rimeldocker/api/GithubAPI.java
@@ -1,29 +1,11 @@
 package fr.polytech.rimel.rimeldocker.api;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import fr.polytech.rimel.rimeldocker.model.CommitHistory;
-import fr.polytech.rimel.rimeldocker.model.Repository;
-import fr.polytech.rimel.rimeldocker.model.tracer.DockerCompose;
-import fr.polytech.rimel.rimeldocker.model.tracer.File;
-import fr.polytech.rimel.rimeldocker.model.tracer.FileTracer;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.joda.time.DateTime;
-import org.joda.time.format.ISODateTimeFormat;
 import org.kohsuke.github.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GithubAPI {
